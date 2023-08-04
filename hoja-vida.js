b8c9935d173b358d542784696dfea443b526b596
eudematos1 <116099820+eudematos1@users.noreply.github.com>
2023-08-04 11:46:35 -0500

    Update hoja-vida.js

--- hoja-vida.js
+++ hoja-vida.js
@@ -1,2 +1,17 @@
 //Crear variables con diferentes tipos de datos (number, string, array, objetos literales) para almacenar información de una persona, 
 //pasar estos como parámetros de una función, que imprimirá en pantalla usando un template string con backcstick (``).
+let fullname = "sonia morales";
+let age = 21;
+let skills = ["dentista", "diseño de modas", "deportista"];
+let address = { mainStreet: "totora", secondaryStreet: "rio malacatus"};
+
+function showCurriculum(name, age, skills, address) {
+    let formatedSkills = skills.join('\n\t');
+    let formatedAddress = `${address.mainStreet}\t${address.secondaryStreet}\t${address.streetNumber}`;
+    let message = `Name: ${name}\nAge: ${age}\nSkills:\n\t${formatedSkills}\nAddress:\n\t${formatedAddress}`;
+    return message;
+}
+
+let curriculum = showCurriculum(fullname, age, skills, address);
+
+console.log(curriculum);
