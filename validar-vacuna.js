5116bc05cb7cec08360365aa93fec1cc40b6531b
eudematos1 <116099820+eudematos1@users.noreply.github.com>
2023-08-04 11:45:50 -0500

    Update validar-vacuna.js

--- validar-vacuna.js
+++ validar-vacuna.js
@@ -2,3 +2,15 @@
 //la función debe validar si la edad aceptado para recibir la vacuna que debe ser 
 //para menores a 5 anos o mayor a 65. La salida de la función es un string de
 //aceptado y no aceptado.
+let person = {name:"sonia", age:21}
+
+function validarvacuna(literalObject){
+    if (literalObject.age<5 || literalObject.age>65){
+        return `${literalObject.name}  aprobado`
+    }
+    return `${literalObject.name} no es aprobado`
+}
+
+let result = validarvacuna(person);
+
+alert(result);
