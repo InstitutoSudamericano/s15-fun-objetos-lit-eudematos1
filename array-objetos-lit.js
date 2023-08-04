23072bf6f097e6c717baa561703230392fad1b90
eudematos1 <116099820+eudematos1@users.noreply.github.com>
2023-08-04 11:46:17 -0500

    Update array-objetos-lit.js

--- array-objetos-lit.js
+++ array-objetos-lit.js
@@ -6,3 +6,16 @@
 //Mi nombre es Juan y tengo 34 anios
 //Mi nombre es Luis y tengo 25 anios
 //Nota: Usar FOR
+let alumnos= [{nombre: "jose", edad: 22},{nombre: "catia", edad: 39}];
+
+function Students(array){
+    let message = "";
+    for (let i =0; i<array.length;i++){
+        message += "Mi nombre es " + alumnos[i].nombre + " y tengo " + alumnos[i].edad + " años \n";
+    }
+    return message;
+}
+
+let lista = Students(alumnos);
+
+console.log(lista)
